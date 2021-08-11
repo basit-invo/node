32ffa3d385a8d5fbc338c47cd17ca93ea814ffdc
Basit Ali <basit.ali@invozone.com>
2021-08-11 16:46:42 +0500

    cron job done, node mailer done

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
+const cron = require('node-cron');
+const mailer = require('nodemailer');
 const router = require('./routes');
 const constants = require('./utils/constants');
 
@@ -6,6 +10,37 @@ const app = express();
 const port = 3000;
 
 console.log(constants.HANAFI);
+
+// Creating a transporter
+const transporter = mailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // use SSL
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+function sendEmail(message) {
+  //sending the email
+  transporter
+    .sendMail({
+      from: '"Basit Ali" <bstjii25@gmail.com>',
+      to: '"You there" <basit.ali@invozone.com>',
+      subject: 'Scheduled Email',
+      text: message,
+    })
+    .then(() => {
+      console.log(`Email sent on ${new Date()}`);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+// cron.schedule('* * * * *', () => {
+//   sendEmail('This is testing');
+// });
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
