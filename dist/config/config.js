6412519a63199a51459a1a464de6ab0ed08e4d8f
Basit Ali <basit.ali@invozone.com>
2021-09-13 19:03:49 +0500

    make better

---
+++ dist/config/config.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    development: {
+        username: 'apple',
+        password: null,
+        database: 'node',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+    },
+    test: {
+        username: 'apple',
+        password: null,
+        database: 'node',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+    },
+    production: {
+        username: 'apple',
+        password: null,
+        database: 'node',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+    },
+};
